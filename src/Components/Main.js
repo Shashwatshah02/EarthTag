39576d666caa56345e54599fd772da66f6df9d5f
unknown <shashwatshah02@gmail.com>
2023-06-15 17:48:41 +0530

    Second Commit

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Logo from "../Images/Logo.jpg";
 import Tree from "../Images/Tree.png";
+import Image from "../Images/Image_Placeholder.jpg";
 import { Card, Container } from "react-bootstrap";
 export default function Main() {
   return (
@@ -27,125 +28,101 @@ export default function Main() {
             />
             planted for free
           </strong>
-          <Container className="col-md-10">
-            <Card
-              style={{
+            <Card className = "col-md-11 mx-auto"  style={{
                 marginTop: "50px",
                 borderRadius: "50px",
-                backgroundColor: "#c8f3fe",
-              }}
+                alignContent:'center',
+                backgroundColor: "#c8f3fe",}}
             >
-              <div className="row">
-                <form action="" className="col-md-6">
-                  <input
-                    type="text"
-                    style={{ height: "50px", width: "100px", margin: "3px" }}
-                  ></input>
-                </form>
-                <Card.Body className="col-md-6">
-                  <strong style={{ textAlign: "left" }}>
-                    10% for you + 10% for
-                  </strong>
-                  <img
-                    src={Tree}
-                    alt=" Tree Logo"
-                    style={{ height: "20px", margin: "0 5px" }}
-                  />
-                </Card.Body>
-              </div>
-            </Card>
-          </Container>
-          <Container className="col-md-10">
-            <Card
+              <Card.Body className="flexbox-container"
               style={{
+                Width:'400px',
+                display:'flex',
+                justifyContent:'space-around'
+              }}>
+                <strong className="flex-item flex-item1">
+                  10% for you + 10% for
+                <img
+                  src={Tree}
+                  alt=" Tree Logo"
+                  style={{ height: "20px", margin: "0 5px" }}
+                  />
+                  </strong>
+                <img className="flex-item flex-item2" src={Image} alt="Placeholder" style={{height:'30px'}}/>
+              </Card.Body>
+            </Card>
+            <Card  className = "col-md-11 mx-auto" style={{
                 marginTop: "20px",
                 borderRadius: "50px",
-                backgroundColor: "#c8f3fe",
-              }}
+                backgroundColor: "#c8f3fe",}}
             >
-              <div className="row">
-                <form action="" className="col-md-6">
-                  <input
-                    type="text"
-                    style={{ height: "50px", width: "100px", margin: "3px" }}
-                  ></input>
-                </form>
-                <Card.Body className="col-md-6">
-                  <strong style={{ textAlign: "left" }}>
-                    10% for you + 10% for
-                  </strong>
-                  <img
-                    src={Tree}
-                    alt=" Tree Logo"
-                    style={{ height: "20px", margin: "0 5px" }}
-                  />
-                </Card.Body>
-              </div>
-            </Card>
-          </Container>
-          <Container className="col-md-10">
-            <Card
+              <Card.Body className="flexbox-container"
               style={{
+                Width:'400px',
+                display:'flex',
+                justifyContent:'space-around'
+              }}>
+                <strong className="flex-item flex-item1">
+                  15% for you + 15% for
+                <img
+                  src={Tree}
+                  alt=" Tree Logo"
+                  style={{ height: "20px", margin: "0 5px" }}
+                  />
+                  </strong>
+                <img className="flex-item flex-item2" src={Image} alt="Placeholder" style={{height:'30px'}}/>
+              </Card.Body>
+            </Card>
+            <Card  className = "col-md-11 mx-auto" style={{
                 marginTop: "20px",
                 borderRadius: "50px",
-                backgroundColor: "#c8f3fe",
-              }}
+                backgroundColor: "#c8f3fe",}}
             >
-              <div className="row">
-                <form action="" className="col-md-6">
-                  <input
-                    type="text"
-                    style={{ height: "50px", width: "100px", margin: "3px" }}
-                  ></input>
-                </form>
-                <Card.Body className="col-md-6">
-                  <strong style={{ textAlign: "left" }}>
-                    10% for you + 10% for
-                  </strong>
-                  <img
-                    src={Tree}
-                    alt=" Tree Logo"
-                    style={{ height: "20px", margin: "0 5px" }}
-                  />
-                </Card.Body>
-              </div>
-            </Card>
-          </Container>
-          <Container className="col-md-10">
-            <Card
+              <Card.Body className="flexbox-container"
               style={{
+                Width:'400px',
+                display:'flex',
+                justifyContent:'space-around'
+              }}>
+                <strong className="flex-item flex-item1">
+                  15% for you + 15% for
+                <img
+                  src={Tree}
+                  alt=" Tree Logo"
+                  style={{ height: "20px", margin: "0 5px" }}
+                  />
+                  </strong>
+                <img className="flex-item flex-item2" src={Image} alt="Placeholder" style={{height:'30px'}}/>
+              </Card.Body>
+            </Card>
+            <Card className = "col-md-11 mx-auto" style={{
                 marginTop: "20px",
                 borderRadius: "50px",
-                backgroundColor: "#c8f3fe",
-              }}
+                backgroundColor: "#c8f3fe",}}
             >
-              <div className="row">
-                <form action="" className="col-md-6">
-                  <input
-                    type="text"
-                    style={{ height: "50px", width: "100px", margin: "3px" }}
-                  ></input>
-                </form>
-                <Card.Body className="col-md-6">
-                  <strong style={{ textAlign: "left" }}>
-                    10% for you + 10% for
-                  </strong>
-                  <img
-                    src={Tree}
-                    alt=" Tree Logo"
-                    style={{ height: "20px", margin: "0 5px" }}
+              <Card.Body className="flexbox-container"
+              style={{
+                Width:'400px',
+                display:'flex',
+                justifyContent:'space-around'
+              }}>
+                <strong className="flex-item flex-item1">
+                  15% for you + 15% for
+                <img
+                  src={Tree}
+                  alt=" Tree Logo"
+                  style={{ height: "20px", margin: "0 5px" }}
                   />
-                </Card.Body>
-              </div>
+                  </strong>
+                <img className="flex-item flex-item2" src={Image} alt="Placeholder" style={{height:'30px'}}/>
+              </Card.Body>
             </Card>
+          <Container style={{ margin: "30px 0 50px 0" }}>
+            <button className="btn mx-auto">Get notified when we add new brands</button>
           </Container>
-          <Container style={{margin:'20px 0 50px 0'}}>
-            <button className="btn" >Get notified when we add new brands</button>
-          </Container>
-          <strong style={{ fontSize: 25, margin:'20px 0'}}>
-            Get to Know us 
-            </strong>
-            <Container className="col-md-10">
+          <strong style={{ fontSize: 25, margin: "20px 0" }}>
+            Get to Know us
+          </strong>
             <Card
               style={{
                 margin: "40px 0 20px 0",
@@ -153,17 +130,13 @@ export default function Main() {
                 backgroundColor: "#fff",
               }}
             >
-              <Card.Body style={{textAlign:'left', paddingLeft:'30px'}}>
+              <Card.Body style={{ textAlign: "left", paddingLeft: "30px" }}>
                 Our, Why how and What
               </Card.Body>
-
             </Card>
+          <Container>
+            <button className="btn mx-auto">Contact Us | Join the ET Communtity</button>
           </Container>
-            <Container>
-            <button className="btn" >Contact Us | Join the ET Communtity</button>
-          </Container>  
-
-          
         </div>
       </div>
     </>
